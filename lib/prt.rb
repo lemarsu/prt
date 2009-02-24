113faca11b8d38420195203bb305248d24062560
LeMarsu <ch.ruelle@lemarsu.com>
2009-02-24 23:12:26 +0100

    Rewritten way to write commands

--- lib/prt.rb
+++ lib/prt.rb
@@ -1,4 +1,4 @@
-class Prt
+class Bin
 
   def usage(exitcode = 1)
     puts <<''
@@ -10,7 +10,7 @@ class Prt
   def main(*args)
     usage 0 if args.size < 1
 
-    commands = public_methods.grep(/^cmd_/).map{|pm| pm.gsub(/^cmd_/,'')}
+    commands = public_methods
     command = args.shift
 
     unless commands.include? command
@@ -22,48 +22,12 @@ class Prt
   end
 
   def run_command(command, args)
-    send(:"cmd_#{command}", *args)
+    send(command, *args)
   end
 
-  def cmd_info(*args)
-    args.each do |port|
-      p port.path
-      p port.installed?
-      p port.installed_version
-      p port.description
-      p port.maintainer
-      p port.url
-      p port.dependencies
-      p port.port_full_version
-      p port.port_remote_sources
-      # p port
-      puts
-    end
-  end
-
-  def cmd_deptree(*args)
-    args.each do |port|
-      puts "Deptre for #{port.name}"
-      CRUX.each_dep port do |port, indent|
-	installed = port.installed? ? "[i]" : "[ ]"
-	puts "#{installed} #{"  " * indent}#{port.name}"
-      end
-    end
-  end
-
-  def cmd_installed(*args)
-    puts "Installed ports :"
-    port_db.ports.each do |port|
-      puts [port.name, port.installed_version].join(' ')
-    end
-  end
-
-  def cmd_remote_sources(*args)
-    args.each do |port|
-      puts "Port #{port.name}"
-      port.port_remote_sources.each do |source|
-	puts "  #{source}"
-      end
+  def self.command(name, opts={}, &blk)
+    define_method(name) do |*args|
+      args.each {|port| blk.call port}
     end
   end
 
@@ -77,6 +41,39 @@ class Prt
       else CRUX::Port.new port_name
       end
     end.flatten
+  end
+
+end
+
+class Prt < Bin
+
+  command :info do |port|
+    p port.path
+    p port.installed?
+    p port.installed_version
+    p port.description
+    p port.maintainer
+    p port.url
+    p port.dependencies
+    p port.port_full_version
+    p port.port_remote_sources
+    # p port
+    puts
+  end
+
+  command :deptree do |port|
+    puts "Deptre for #{port.name}"
+    CRUX.each_dep port do |port, indent|
+      installed = port.installed? ? "[i]" : "[ ]"
+      puts "#{installed} #{"  " * indent}#{port.name}"
+    end
+  end
+
+  command :remote_sources do |port|
+    puts "Port #{port.name}"
+    port.port_remote_sources.each do |source|
+      puts "  #{source}"
+    end
   end
 
 end
