9f64cb1e170e63eb34d4143c44de87f8930724e0
LeMarsu <ch.ruelle@lemarsu.com>
2009-02-24 22:20:05 +0100

    Moving methods to CRUX module

--- lib/crux.rb
+++ lib/crux.rb
@@ -1,8 +1,10 @@
-module CRUX
-
-end
-
 require 'crux/port'
 require 'crux/port_db'
 require 'crux/port_dir'
 require 'crux/prt_get_conf'
+
+module CRUX
+  def self.each_installed_ports(&blk)
+    PortDB.new.ports.each(&blk)
+  end
+end
